55be380c83f7d96d7607456a498bc19cf1435d14
mjguggen <mjguggen@gmail.com>
2020-03-18 14:01:31 -0500

    Updates

---
+++ precache-manifest.b741fbc066e7f0e9468281326c039593.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c660338ebf9aa984a623151e5a6cf39",
+    "url": "/Weather-Forecast/index.html"
+  },
+  {
+    "revision": "64e9b81b51345bd6ebea",
+    "url": "/Weather-Forecast/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b7d4f2786fac7aa6a79a",
+    "url": "/Weather-Forecast/static/css/main.a7213ed7.chunk.css"
+  },
+  {
+    "revision": "64e9b81b51345bd6ebea",
+    "url": "/Weather-Forecast/static/js/2.b1ef1298.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Weather-Forecast/static/js/2.b1ef1298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d4f2786fac7aa6a79a",
+    "url": "/Weather-Forecast/static/js/main.41fb350a.chunk.js"
+  },
+  {
+    "revision": "6cbc84f3eedb80ac938b",
+    "url": "/Weather-Forecast/static/js/runtime-main.3e842584.js"
+  }
+]);
